016bd84b1f4069a043e0c2602d4d3fcb7079a952
137725897+chadzimmerman@users.noreply.github.com <bookingpedestals@gmail.com>
2026-01-14 13:42:11 +0300

    Changed the remainder of file and folder name spaces to underscores. Set up pages on home router for all 11 pages. Set up pages 5-7.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ import PortfolioPage1 from "./pages/Project-1.jsx";
 import PortfolioPage2 from "./pages/Project-2.jsx";
 import PortfolioPage3 from "./pages/Project-3.jsx";
 import PortfolioPage4 from "./pages/Project-4.jsx";
+import PortfolioPage5 from "./pages/Project-5.jsx";
+import PortfolioPage6 from "./pages/Project-6.jsx";
+import PortfolioPage7 from "./pages/Project-7.jsx";
+import PortfolioPage8 from "./pages/Project-8.jsx";
+import PortfolioPage9 from "./pages/Project-9.jsx";
+import PortfolioPage10 from "./pages/Project-10.jsx";
+import PortfolioPage11 from "./pages/Project-11.jsx";
 
 function App() {
   return (
@@ -22,6 +29,13 @@ function App() {
         <Route path="/texas-neighborhood-2" element={<PortfolioPage2 />} />
         <Route path="/darul-huda-masjid-mosque" element={<PortfolioPage3 />} />
         <Route path="/greek-villa" element={<PortfolioPage4 />} />
+        <Route path="/pine-woods" element={<PortfolioPage5 />} />
+        <Route path="/california-interior" element={<PortfolioPage6 />} />
+        <Route path="/mariya-villa" element={<PortfolioPage7 />} />
+        <Route path="/portland-residence" element={<PortfolioPage8 />} />
+        <Route path="/office-1" element={<PortfolioPage9 />} />
+        <Route path="/office-2" element={<PortfolioPage10 />} />
+        <Route path="/scandinavian-house" element={<PortfolioPage11 />} />
       </Routes>
     </Layout>
   );
